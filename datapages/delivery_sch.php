cb68ff8a5b03d8171607cfd87a0c5bc075220d60
aakanksha-ui <aakanksha@machinemaze.com>
2024-07-30 10:15:48 +0530

    delivery_sch

--- datapages/delivery_sch.php
+++ datapages/delivery_sch.php
@@ -148,7 +148,7 @@
                           <div class="col mr-2">
                               <div class="text-xs fw-bold text-uppercase mb-2 text-light"> Avg.Rate of Acceptance
                               </div>
-                              <div class="h5 mb-0 fw-bold text-light"><?php echo $summaryDetails['data'][0]['Avg_Acceptance_Rate'] ?> %</div>
+                              <div class="h5 mb-0 fw-bold text-light"><?php echo $summaryDetails['data'][0]['Avg_Acceptance_Rate'] * 100 ?> %</div>
                               <div class="mt-2 mb-0 text-dark text-xs">                     <!--     <span class="text-dark mr-2"><i class="fas fa-arrow-up"></i> <b> ( ₹ 0 )</b> </span>  <span>Since last years</span>         -->               </div>
                           </div>
                           <div class="col-auto icnCircle bg-transparent">                        
